2ff91b3ccd449824c027f6e0d33d5de3de6fec78
kravtsov <shrituon@gmail.com>
2023-10-05 10:34:19 +0300

    added kafka config as publish, otherwise it's not working

--- src/KafkaServiceProvider.php
+++ src/KafkaServiceProvider.php
@@ -11,10 +11,19 @@ class KafkaServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->publishesConfiguration();
+
         $manager = $this->app['queue'];
 
         $manager->addConnector('kafka', function () {
             return new KafkaConnector;
         });
+    }
+
+    private function publishesConfiguration(): void
+    {
+        $this->publishes([
+            __DIR__."/../../config/kafka.php" => config_path('kafka.php'),
+        ], 'kafka-queue-config');
     }
 }
