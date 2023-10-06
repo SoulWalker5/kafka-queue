017774459fce2842ac5f0e128d90377c8add058e
kravtsov <shrituon@gmail.com>
2023-10-06 10:10:00 +0300

    changed kafka-queue to work with different topics as with queue names

--- src/KafkaConnector.php
+++ src/KafkaConnector.php
@@ -3,40 +3,11 @@
 namespace DiKay\Kafka;
 
 use Illuminate\Queue\Connectors\ConnectorInterface;
-use Illuminate\Support\Facades\Log;
-use Junges\Kafka\Config\Sasl;
-use Junges\Kafka\Contracts\KafkaConsumerMessage;
-use Junges\Kafka\Facades\Kafka;
 
 class KafkaConnector implements ConnectorInterface
 {
     public function connect(array $config)
     {
-        $producer = Kafka::publishOn($config['queue'], $config['bootstrap_servers'])
-            ->withSasl(new Sasl(
-                $config['sasl_username'],
-                $config['sasl_password'],
-                $config['sasl_mechanism'],
-                $config['security_protocol'],
-            ));
-
-        $consumer = Kafka::createConsumer([$config['bootstrap_servers']])
-            ->withSasl(new Sasl(
-                $config['sasl_username'],
-                $config['sasl_password'],
-                $config['sasl_mechanism'],
-                $config['security_protocol'],
-            ))
-            ->withHandler(function(KafkaConsumerMessage $message) {
-                $job = unserialize($message->getBody());
-                $job->handle();
-
-                $mes = is_string($message->getBody()) ? $message->getBody() : json_encode($message->getBody()) ;
-                Log::info('Consumed message: ' . $mes);
-                echo "Consumed message: $mes\n";
-            })
-            ->build();
-
-        return new KafkaQueue($consumer, $producer);
+        return new KafkaQueue($config);
     }
 }
--- src/KafkaQueue.php
+++ src/KafkaQueue.php
@@ -7,17 +7,28 @@ use Exception;
 use Illuminate\Contracts\Queue\Queue as QueueContract;
 use Illuminate\Queue\Queue;
 use Illuminate\Support\Facades\Log;
-use Junges\Kafka\Contracts\CanConsumeMessages;
-use Junges\Kafka\Contracts\CanProduceMessages;
+use Junges\Kafka\Config\Sasl;
+use Junges\Kafka\Contracts\KafkaConsumerMessage;
 use Junges\Kafka\Exceptions\KafkaConsumerException;
+use Junges\Kafka\Facades\Kafka;
 use Junges\Kafka\Message\Message;
 
 class KafkaQueue extends Queue implements QueueContract
 {
-    public function __construct(
-        protected readonly CanConsumeMessages $consumer,
-        protected readonly CanProduceMessages $producer,
-    ) {
+    protected string $bootstrapServers;
+    protected string $queue;
+    protected Sasl $sasl;
+
+    public function __construct(array $config)
+    {
+        $this->bootstrapServers = $config['bootstrap_servers'];
+        $this->queue = $config['queue'];
+        $this->sasl = new Sasl(
+            $config['sasl_username'],
+            $config['sasl_password'],
+            $config['sasl_mechanism'],
+            $config['security_protocol'],
+        );
     }
 
     public function size($queue = null)
@@ -28,7 +39,10 @@ class KafkaQueue extends Queue implements QueueContract
     public function push($job, $data = '', $queue = null)
     {
         try {
-            $this->producer->withMessage(new Message(body: serialize($job)))->send();
+            Kafka::publishOn($queue ?? $this->queue, $this->bootstrapServers)
+                ->withSasl($this->sasl)
+                ->withMessage(new Message($queue, body: serialize($job)))
+                ->send();
         } catch (Exception $e) {
             Log::error(self::class . ' ' . $e->getMessage());
         }
@@ -47,9 +61,26 @@ class KafkaQueue extends Queue implements QueueContract
     public function pop($queue = null)
     {
         try {
-            $this->consumer->consume();
+            $consumer = Kafka::createConsumer([$queue ?? $this->queue], brokers: $this->bootstrapServers)
+                ->withSasl($this->sasl)
+                ->withHandler($this->jobHandler())
+                ->build();
+
+            $consumer->consume();
         } catch (CarbonException|KafkaConsumerException $e) {
             Log::error(self::class . ' ' . $e->getMessage());
         }
+    }
+
+    protected function jobHandler(): callable
+    {
+        return function(KafkaConsumerMessage $message) {
+            $job = unserialize($message->getBody());
+            $job->handle();
+
+            $mes = is_string($message->getBody()) ? $message->getBody() : json_encode($message->getBody()) ;
+            Log::info('In topic '. $message->getTopicName() .' consumed message: ' . $mes);
+            echo "In topic {$message->getTopicName()} consumed message: $mes\n";
+        };
     }
 }
